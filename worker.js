fee65e5c7b3dcc28931db3910435508bb5339dc0
Audun Kvasbø <audun@kvasbo.no>
2023-07-22 09:21:34 +0200

    Add source link and worker code

--- worker.js
+++ worker.js
@@ -1,26 +1,24 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run "npm run dev" in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run "npm run deploy" to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
   async fetch(request, env, ctx) {
-    var nowDate = new Date();
-    var moenDate = new Date("2023-07-21T00:00:00Z");
-    var brennaDate = new Date("2023-06-20T00:00:00Z"); 
-    var tretteDate = new Date("2023-06-23T00:00:00Z"); 
-    var diffInDaysMoen = Math.floor((nowDate - moenDate) / (1000 * 60 * 60 * 24));
-    var diffInDaysBrenna = Math.floor((nowDate - brennaDate) / (1000 * 60 * 60 * 24));
-    var diffInDaysTrette = Math.floor((nowDate - tretteDate) / (1000 * 60 * 60 * 24));
-    
+    const now = new Date();
+
+    const cases = [
+      "2023-07-21T00:00:00Z",
+      "2023-06-20T00:00:00Z",
+      "2023-06-23T00:00:00Z"
+    ];
+
+   const counts = cases.map((d) => {
+    const dt = new Date(d);
+    return Math.floor((now - dt) / (1000 * 60 * 60 * 24));
+   });
+
+   counts.sort();
+   const latest = counts.shift();
+
     var out = {
-      latest: diffInDaysMoen,
-      older: [diffInDaysTrette, diffInDaysBrenna],
+      latest: latest,
+      older: counts
     };
 
     const headers = new Headers();
